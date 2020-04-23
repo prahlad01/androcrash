896a73df3c8d66a4ce287e24437080d99a49d8a3
prahlad01 <43715907+prahlad01@users.noreply.github.com>
2020-04-24 03:16:31 +0530

    Update crash.sh

--- crash.sh
+++ crash.sh
@@ -131,8 +131,8 @@ read choice
 if [ $choice = 't' ] ; then
 echo -e "$red                 Are you sure? Press$grn ENTER$red to exit$rset"
 read monn
-cd $HOME/Infect
-bash infect.sh
+cd $HOME/crash
+bash crash.sh
 else
 bash update.sh
 echo " "
